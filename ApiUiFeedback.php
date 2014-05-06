4b379309a2e0c16d53716ccd73f4dd942ebee1bf
Fomafix <fomafix@googlemail.com>
2014-05-06 02:30:25 +0000

    Apply coding conventions
    * Encode U+1F4F7 as \1f4f7 in CSS
    
    Change-Id: I2d4f808065fc1a2bbba7c4dca0c08fd290d084ab

--- ApiUiFeedback.php
+++ ApiUiFeedback.php
@@ -107,9 +107,9 @@ class UiFeedbackAPI extends ApiBase {
 			/* update table */
 			$dbw = wfGetDB( DB_MASTER );
 			$dbw->update( 'uifeedback_stats',
-						  $value,
-						  array( 'uifs_type' => $type ),
-						  __METHOD__
+				$value,
+				array( 'uifs_type' => $type ),
+				__METHOD__
 			);
 
 			$this->getResult()->addValue( null, $this->getModuleName(), array( 'status' => 'ok' ) );
@@ -131,9 +131,9 @@ class UiFeedbackAPI extends ApiBase {
 			$dbw->update( 'uifeedback', $values, $conds, __METHOD__, array() );
 
 			$values = array( 'uifr_feedback_id' => $id,
-							 'uifr_reviewer'    => $reviewer,
-							 'uifr_status'      => $new_status,
-							 'uifr_comment'     => $comment
+				'uifr_reviewer'    => $reviewer,
+				'uifr_status'      => $new_status,
+				'uifr_comment'     => $comment
 			);
 			$dbw->insert( 'uifeedback_reviews', $values, __METHOD__, array() );
 
@@ -159,33 +159,48 @@ class UiFeedbackAPI extends ApiBase {
 	public function getAllowedParams() {
 		return array(
 			'mode'                   => array(),
-			'ui-feedback-anonymous'  => array( ApiBase::PARAM_TYPE => 'boolean',
-											   ApiBase::PARAM_DFLT => false ),
+			'ui-feedback-anonymous'  => array(
+				ApiBase::PARAM_TYPE => 'boolean',
+				ApiBase::PARAM_DFLT => false ),
 			'ui-feedback-username'   => array(),
 			'ui-feedback-notify'     => array(),
 			'ui-feedback-task'       => array(),
-			'ui-feedback-task-other' => array( ApiBase::PARAM_TYPE => 'string',
-											   ApiBase::PARAM_DFLT => null ),
-			'ui-feedback-done'       => array( ApiBase::PARAM_TYPE => 'string', /* i took string here because boolean defaults to false when not set */
-											   ApiBase::PARAM_DFLT => null ),
-			'ui-feedback-type'       => array( ApiBase::PARAM_TYPE => array( '0', '1' ) ),
+			'ui-feedback-task-other' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_DFLT => null ),
+			'ui-feedback-done'       => array(
+				ApiBase::PARAM_TYPE => 'string', /* i took string here because boolean defaults to false when not set */
+				ApiBase::PARAM_DFLT => null ),
+			'ui-feedback-type'       => array(
+				ApiBase::PARAM_TYPE => array( '0', '1' ) ),
 			'ui-feedback-url'        => array(),
-			'ui-feedback-importance' => array( ApiBase::PARAM_TYPE => array( '0', '1', '2', '3', '4', '5' ),
-											   ApiBase::PARAM_DFLT => '0' ),
+			'ui-feedback-importance' => array(
+				ApiBase::PARAM_TYPE => array( '0', '1', '2', '3', '4', '5' ),
+				ApiBase::PARAM_DFLT => '0' ),
 			'ui-feedback-url'        => array(),
-			'ui-feedback-happened'   => array( ApiBase::PARAM_TYPE => array( '0', '1', '2', '3', '4' ),
-											   ApiBase::PARAM_DFLT => '0' ),
-			'ui-feedback-text1'      => array( ApiBase::PARAM_TYPE => 'string' ),
-			'ui-feedback-useragent'  => array( ApiBase::PARAM_TYPE => 'string' ),
+			'ui-feedback-happened'   => array(
+				ApiBase::PARAM_TYPE => array( '0', '1', '2', '3', '4' ),
+				ApiBase::PARAM_DFLT => '0' ),
+			'ui-feedback-text1'      => array(
+				ApiBase::PARAM_TYPE => 'string' ),
+			'ui-feedback-useragent'  => array(
+				ApiBase::PARAM_TYPE => 'string' ),
 			'file'                   => array(),
-			'id'                     => array( ApiBase::PARAM_TYPE => 'integer',
-											   ApiBase::PARAM_MIN  => 1 ),
-			'status'                 => array( ApiBase::PARAM_TYPE => array( '1', '2', '3' ) ),
-			'comment'                => array( ApiBase::PARAM_TYPE => 'string' ),
-			'type'                   => array( ApiBase::PARAM_TYPE => array( '0', '1', '2' ) ),
-			'click'                  => array( ApiBase::PARAM_TYPE => 'integer' ),
-			'show'                   => array( ApiBase::PARAM_TYPE => 'integer' ),
-			'sent'                   => array( ApiBase::PARAM_TYPE => 'integer' ),
+			'id'                     => array(
+				ApiBase::PARAM_TYPE => 'integer',
+				ApiBase::PARAM_MIN  => 1 ),
+			'status'                 => array(
+				ApiBase::PARAM_TYPE => array( '1', '2', '3' ) ),
+			'comment'                => array(
+				ApiBase::PARAM_TYPE => 'string' ),
+			'type'                   => array(
+				ApiBase::PARAM_TYPE => array( '0', '1', '2' ) ),
+			'click'                  => array(
+				ApiBase::PARAM_TYPE => 'integer' ),
+			'show'                   => array(
+				ApiBase::PARAM_TYPE => 'integer' ),
+			'sent'                   => array(
+				ApiBase::PARAM_TYPE => 'integer' ),
 
 		);
 	}
