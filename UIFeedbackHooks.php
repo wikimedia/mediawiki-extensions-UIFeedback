997b84a72392d4ed91d45e89606127f5e7910163
Zoranzoki21 <zorandori4444@gmail.com>
2020-01-08 16:15:22 +0000

    Convert UIFeedback to use extension registration
    Bug: T237828
    Change-Id: I436730b7bbbc63f369333579e4dc50c3cc06f89b

---
+++ UIFeedbackHooks.php
@@ -0,0 +1,19 @@
+<?php
+
+class UIFeedbackHooks {
+	function createUIFeedbackTable( DatabaseUpdater $updater ) {
+		$updater->addExtensionTable( 'uifeedback',
+			__DIR__ . '/table.sql', true );
+		return true;
+	}
+	function uifeedbackBeforePageDisplay( &$out ) {
+		if ( $out->getUser()->isAllowed( 'read_uifeedback' ) ) {
+			$out->addModules( [
+				'ext.uiFeedback',
+				'jquery.ui.draggable',
+				'jquery.ui.resizable'
+			] );
+		}
+	return true;
+	}
+}
